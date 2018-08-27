a16ed46b841175d75303bdc82d64699c5b5b5345
AeranicusCascadia <35699755+AeranicusCascadia@users.noreply.github.com>
2018-08-27 16:21:33 -0700

    resume use of tabs instead of spaces

--- adventure.py
+++ adventure.py
@@ -3,80 +3,87 @@ import textwrap
 # command lists and aliases
 list_move_commands = ['north', 'south', 'east', 'west']
 move_alias_dict = {
-    'n' : 'north',
-    's' : 'south',
-    'e' : 'east',
-    'w' : 'west',
-    'u' : 'up',
-    'd' : 'down'
+	'n' : 'north',
+	's' : 'south',
+	'e' : 'east',
+	'w' : 'west',
+	'u' : 'up',
+	'd' : 'down'
 }
 
 # Classes
 
 class Game:
-    def __init__(self):
-        self.running = True
+	def __init__(self):
+		self.running = True
 		
 class Room:
-    def __init__(self, name):
-        self.name = name
-        self.description = None
-        self.exits = {
-            'north' : None,
-            'south' : None,
-            'east' : None,
-            'west' : None
-        }
+	def __init__(self, name):
+		self.name = name
+		self.description = None
+		self.exits = {
+			'north' : None,
+			'south' : None,
+			'east' : None,
+			'west' : None
+		}
 		
-    def describe(self):
-        print('')
-        print(self.name)
-        print('=' * len(self.name))
+	def describe(self):
+		print('')
+		print('=' * len(self.name))
+		print(self.name)
+		print('=' * len(self.name))
+		print('')
+		for line in textwrap.wrap(self.description, 80):
+			print(line)
 		
-        for line in textwrap.wrap(self.description, 80):
-            print(line)
-		
-        print('')
-		
-        print('Exits from here:')
-        print('----------------')
-        for k, v in self.exits.items():
-            if v == None:
-                print(k + ':', 'none visible')
-            else:
-                print(k + ':', v.name)
+	def show_exits(self):
+		print('')	
+		print('Exits from here:')
+		print('----------------')
+		for k, v in self.exits.items():
+			if v == None:
+				print(k + ':', 'none visible')
+			else:
+				print(k + ':', v.name)
 				
 class Player:
-    def __init__(self):
-        self.location = None
+	def __init__(self):
+		self.location = None
+		self.inventory = []
+
+	# Player movement method	
+	def move(self, move_command): # Outer method
 	
-    # Player movement method	
-    def move(self, move_command): # Outer method
-	
-        def execute_move(self, destination): # Inner method
-            if destination in self.location.exits and self.location.exits[move_command] != None:
-                self.location = self.location.exits[move_command]
-            else:
-                print('')
-                print('That does not seem to be a viable exit from here.')
+		def execute_move(self, destination): # Inner method
+			if destination in self.location.exits and self.location.exits[move_command] != None:
+				self.location = self.location.exits[move_command]
+			else:
+				print('That does not seem to be a viable exit from here.')
 				
-        print('')
-        
-        if len(move_command) < 2 and move_command in move_alias_dict:
-            move_command = move_alias_dict[move_command]
+		print('')
+
+		if len(move_command) < 2 and move_command in move_alias_dict:
+			move_command = move_alias_dict[move_command]
 			
-        execute_move(self, move_command)
-		
-    # General command input method 
-    def get_command(self):
-	    print('')
-	    command = input('What is your command? ')
-	    command = command.lower()
-	    if command in list_move_commands or command in move_alias_dict:
-		    self.move(command)
-	    else:
-		    print('That looks like a commmand other than movement.')
-		
+		execute_move(self, move_command)
+	
+	# General command input method 
+	def get_command(self):
+		print('')
+		command = input('What is your command? ')
+		command = command.lower()
+		if command in list_move_commands or command in move_alias_dict:
+			self.move(command)
+		else:
+			print('')
+			print('That looks like a commmand other than movement.')
+class Object:
+	def __init__(self, name):
+		self.name = name
+		self.description = None
+		self.takeable = False
+			
 		
 # Instatiate rooms		
 town_square = Room('Town Square')
@@ -85,20 +92,21 @@ general_store = Room('General Store')
 # Declare room properties
 town_square.description = "The central town square has a smattering of people going about their daily business."
 town_square.exits = {
-		    'north' : general_store,
-		    'south' : None,
-		    'east' : None,
-		    'west' : None
-        }
+			'north' : general_store,
+			'south' : None,
+			'east' : None,
+			'west' : None
+		}
 		
 general_store.description = "This humble structure serves as the town's only shop."
 general_store.exits = {
-		    'north' : None,
-		    'south' : town_square,
-		    'east' : None,
-		    'west' : None
-        }
-				
+			'north' : None,
+			'south' : town_square,
+			'east' : None,
+			'west' : None
+		}
+# Instantiate objects
+			
 	
 # Instantiate game and player
 game = Game()
@@ -107,9 +115,10 @@ player.location = town_square
 
 # Game loop
 while (game.running == True):
-    player.location.describe()
-    player.get_command()
- 
+	player.location.describe()
+	player.location.show_exits()
+	player.get_command()
+
 
 
 
